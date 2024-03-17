18fb6b74db60559d257360aace570dcfed9a982a
RoxyWilliams <roxylamuza16@gmail.com>
2024-03-17 17:34:20 -0600

    Font and Images

--- PuraVidaDelivery/PuraVidaDelivery/AssemblyInfo.cs
+++ PuraVidaDelivery/PuraVidaDelivery/AssemblyInfo.cs
@@ -1,3 +1,11 @@
-﻿using Xamarin.Forms.Xaml;
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+
+[assembly:ExportFont ("Tuna Sandwich.ttf", Alias = "TunaSand")]
+[assembly: ExportFont("Lisboa Sans LF Bold.otf", Alias = "Lisboa Sans")]
+[assembly: ExportFont("Lisboa Sans OSF Regular.otf", Alias = "Lisboa Sans Reg")]
+[assembly: ExportFont("BentonSans Book.otf", Alias = "BentonSans")]
+[assembly: ExportFont("BentonSans Bold Regular.otf", Alias = "BentonSans Bold")]
+[assembly: ExportFont("BentonSans Medium Regular.otf", Alias = "BentonSans Medium")]
